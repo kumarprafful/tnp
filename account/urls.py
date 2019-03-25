b68e461a8e22f3e954d4b91d5e627151c1a53141
Prafful Kumar <kumarprafful.54@gmail.com>
2019-03-26 03:09:00 +0530

    faculty auth ui

--- account/urls.py
+++ account/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register/', account_views.student_register, name='student_register'),
     path('login/', account_views.student_login, name='student_login'),
     path('logout/', account_views.user_logout, name='logout'),
-    path('faculty_register', account_views.faculty_register, name='faculty_register'),
-    path('faculty_login', account_views.faculty_login, name='faculty_login'),
+    path('faculty_register/', account_views.faculty_register, name='faculty_register'),
+    path('faculty_login/', account_views.faculty_login, name='faculty_login'),
 ]
