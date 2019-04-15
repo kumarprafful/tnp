075bdade152ed5c596dbc58f85a8aa00e50a348c
akshatpapnoi <akshatpapnoi1110@gmail.com>
2019-04-16 02:47:09 +0530

    forgit password done

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse, reverse_lazy
 from account import views as account_views
+from django.contrib.auth import views as auth_views
 
 app_name = 'account'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('logout/', account_views.user_logout, name='logout'),
     path('faculty_register/', account_views.faculty_register, name='faculty_register'),
     path('faculty_login/', account_views.faculty_login, name='faculty_login'),
+ 
 ]
